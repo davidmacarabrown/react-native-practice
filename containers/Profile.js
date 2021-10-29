059879575bdcf9cbc7aad94c72c3295cbdc83cfa
davidmacarabrown <davidmacarabrown@gmail.com>
2021-10-29 22:53:26 +0100

    add: improving spacing of items on profile screen via flex

--- containers/Profile.js
+++ containers/Profile.js
@@ -5,21 +5,29 @@ export default function Profile ({props}) {
 
     console.log("================", props, "=========================")
     return(
-            <View style={styles.container}>
+            <View>
 
                 <ImageBackground source={require('../assets/images/smiley-g55c246914_1280.png')} style={styles.backgroundImage}>
 
                     <View>
+                        <Text style={styles.textLarge}>{props.username}</Text>
+                    </View>
 
-                        <View style={styles.profileWrapper}>
-                            <Text style={styles.textLevelBox}>Level</Text>
-                            <Text style={styles.textLevel}>{props.level}</Text>
+                    <View style={styles.infoContainer}>
+
+                        <View style={styles.levelContainer}>
+                            <Text style={styles.textSmall}>Level</Text>
+                            <Text style={styles.textLarge}>{props.level}</Text>
                         </View>
 
-                        <View style={styles.profileWrapper2}>
-                            <Text style={styles.textHealthBox}>Health</Text>
-                            <Text style={styles.textHealth}>{props.health}</Text>
-                            
+                        <View style={styles.xpContainer}>
+                            <Text style={styles.textSmall}>XP</Text>
+                            <Text style={styles.textLarge}>{props.currentXp}/{props.maximumXp}</Text>
+                        </View>
+
+                        <View style={styles.healthContainer}>
+                            <Text style={styles.textSmall}>Health</Text>
+                            <Text style={styles.textLarge}>{props.health}</Text>
                         </View>
 
                     </View>
@@ -32,55 +40,44 @@ export default function Profile ({props}) {
 
 const styles = StyleSheet.create({
 
-    container:{
-        flex:1,
-        backgroundColor:'white'
-    },
-
     backgroundImage:{
         width: '100%',
         aspectRatio: 1,
-        alignContent: 'space-around'
+        flexDirection: 'column',
+        justifyContent: 'space-between'
     },
-
-    profileWrapper:{
+    
+    infoContainer:{
         position: 'absolute',
-        left: 0,
         bottom: 0,
-        marginHorizontal:20,
-        marginBottom:10,
-       
-    },
-    profileWrapper2:{
-        position:'absolute',
-        right:0,
-        margin:10,
-        marginHorizontal:20,
-        marginBottom:10,
-        bottom:0,
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-start'
     },
 
-    textLevel:{
-        fontFamily:'Raleway-Bold',
-        fontSize:32,
-        color:'purple',
+    levelContainer:{
+        flex: 1
     },
-    textLevelBox:{
+
+    xpContainer: {
+        flex: 1
+    },
+
+    healthContainer: {
+        flex: 1
+    },
+    
+    textSmall:{
         fontFamily:'Raleway-Bold',
         fontSize:18,
         color:'purple',
+        textAlign: 'center'
     },
-    textHealth:{
+
+    textLarge:{
         fontFamily:'Raleway-Bold',
         fontSize:32,
         color:'purple',
-
-    },
-    textHealthBox:{
-        fontFamily:'Raleway-Bold',
-        fontSize:18,
-        color:'purple',
-        justifyContent:'space-between',
+        textAlign: 'center'
     }
-
 })  
